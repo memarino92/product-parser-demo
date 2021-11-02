769a5f6a394c78cc0b24758fdcf0f04b50f81574
Michael Marino <memarino92@gmail.com>
2021-11-02 14:05:50 -0400

    added remove first x char functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import './App.css'
 
 
 function App() {
+  
+  //define state
   const [inputText, setInputText] = useState('')
-  const [inputNumber, setInputNumber] = useState(0)
+  const [inputNumberToRemove, setInputNumberToRemove] = useState(0)
+  const [inputNumberToKeep, setInputNumberToKeep] = useState(0)
   const [leftOrRight, setLeftOrRight] = useState('Left')
   const [stripSpecialChar, setStripSpecialChar] = useState(false)
 
+  //define event handlers
   const handleSideChange = (e) => {
     setLeftOrRight(e.target.value)
   }
@@ -17,8 +21,12 @@ function App() {
     setInputText(e.target.value)
   }
 
-  const handleNumberInputChange = (e) => {
-    setInputNumber(e.target.value)
+  const handleNumberToKeepInputChange = (e) => {
+    setInputNumberToKeep(e.target.value)
+  }
+
+  const handleNumberToRemoveInputChange = (e) => {
+    setInputNumberToRemove(e.target.value)
   }
 
   const handleStripSpecialCharChange = (e) => {
@@ -31,16 +39,17 @@ function App() {
 
   //This is where parsing is implemented based on user-defined rules
   const parseInput = (input) => {
-    let parsedInput = input
+    
+    let parsedInput = input.slice(inputNumberToRemove)
 
     if (stripSpecialChar) {
       parsedInput = parsedInput.replace(/[^a-zA-Z0-9]/g, '')
     }
     if (leftOrRight == 'Left') {
-      parsedInput = parsedInput.slice(0, inputNumber)
+      parsedInput = parsedInput.slice(0, inputNumberToKeep)
     } 
     if (leftOrRight == 'Right') {
-      parsedInput = parsedInput.slice(-inputNumber)
+      parsedInput = parsedInput.slice(-inputNumberToKeep)
     }
     
     return parsedInput
@@ -52,18 +61,21 @@ function App() {
       <div className="container">
         <form onSubmit={preventSubmit}>
           
-          <p>I want to keep the</p>
-          <select name="leftOrRight" id="side" onChange={handleSideChange}>
+          <p>I want to remove the first </p>
+          <input type="number" placeholder="Enter a number" onChange={handleNumberToRemoveInputChange}/>
+          <br/>
+          <p>characters and then keep the</p>
+          <select onChange={handleSideChange}>
               <option value="Left">Left</option>
               <option value="Right">Right</option>
-          </select>
-          
-          <input type="number" id="numberInput" placeholder="Enter a number" onChange={handleNumberInputChange}/>
+          </select>  {''}
+         
+          <input type="number" placeholder="Enter a number" onChange={handleNumberToKeepInputChange}/>
           <br/>
           <p>characters</p>
           
-          <input type="checkbox" id="stripSpecialChar" name="stripSpecialChar" onChange={handleStripSpecialCharChange}/>
-          <label htmlFor="stripSpecialChar">Strip Special Characters</label>
+          <input type="checkbox" onChange={handleStripSpecialCharChange}/>
+          <label>Strip Special Characters</label>
           <br/>
           
           <input id="textInput" placeholder="Scan serial number here" onChange={handleTextInputChange}/>
