1b1352b9400157f6350a445f769c4a627f1ca4ad
Michael Marino <memarino92@gmail.com>
2021-11-02 15:00:49 -0400

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,93 +1,78 @@
-import React from 'react'
-import { useState } from 'react'
-import './App.css'
-
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
   //define state
-  const [inputText, setInputText] = useState('')
-  const [inputNumberToRemove, setInputNumberToRemove] = useState(0)
-  const [inputNumberToKeep, setInputNumberToKeep] = useState(0)
-  const [leftOrRight, setLeftOrRight] = useState('Left')
-  const [stripSpecialChar, setStripSpecialChar] = useState(false)
+  const [numberToRemove, setNumberToRemove] = useState(0);
+  const [leftOrRight, setLeftOrRight] = useState("Left");
+  const [numberToKeep, setNumberToKeep] = useState(0);
+  const [stripSpecialChar, setStripSpecialChar] = useState(false);
+  const [inputText, setInputText] = useState("");
 
   //define event handlers
-  const handleSideChange = (e) => {
-    setLeftOrRight(e.target.value)
-  }
-
-  const handleTextInputChange = (e) => {
-    setInputText(e.target.value)
-  }
-
-  const handleNumberToKeepInputChange = (e) => {
-    setInputNumberToKeep(e.target.value)
-  }
-
-  const handleNumberToRemoveInputChange = (e) => {
-    setInputNumberToRemove(e.target.value)
-  }
-
-  const handleStripSpecialCharChange = (e) => {
-    setStripSpecialChar(e.target.checked)
-  }
-  
-  //Prevent submit from clearing data
-  const preventSubmit = (e) => e.preventDefault()
-
+  const handleNumberToRemoveChange = (e) => setNumberToRemove(e.target.value);
+  const handleLeftOrRightChange = (e) => setLeftOrRight(e.target.value);
+  const handleNumberToKeepChange = (e) => setNumberToKeep(e.target.value);
+  const handleStripSpecialCharChange = (e) =>
+    setStripSpecialChar(e.target.checked);
+  const handleInputTextChange = (e) => setInputText(e.target.value);
 
   //This is where parsing is implemented based on user-defined rules
   const parseInput = (input) => {
-    
-    let parsedInput = input.slice(inputNumberToRemove)
+    let parsedInput = input.slice(numberToRemove);
 
     if (stripSpecialChar) {
-      parsedInput = parsedInput.replace(/[^a-zA-Z0-9]/g, '')
+      parsedInput = parsedInput.replace(/[^a-zA-Z0-9]/g, "");
     }
-    if (leftOrRight == 'Left') {
-      parsedInput = parsedInput.slice(0, inputNumberToKeep)
-    } 
-    if (leftOrRight == 'Right') {
-      parsedInput = parsedInput.slice(-inputNumberToKeep)
+    if (leftOrRight == "Left") {
+      parsedInput = parsedInput.slice(0, numberToKeep);
     }
-    
-    return parsedInput
-  }
-  
+    if (leftOrRight == "Right") {
+      parsedInput = parsedInput.slice(-numberToKeep);
+    }
+
+    return parsedInput;
+  };
 
   return (
     <>
       <div className="container">
-        <form onSubmit={preventSubmit}>
-          
+        <form onSubmit={(e) => e.preventDefault()}>
           <p>I want to remove the first </p>
-          <input type="number" placeholder="Enter a number" onChange={handleNumberToRemoveInputChange}/>
-          <br/>
+          <input
+            type="number"
+            placeholder="Enter a number"
+            onChange={handleNumberToRemoveChange}
+          />
+          <br />
           <p>characters and then keep the</p>
-          <select onChange={handleSideChange}>
-              <option value="Left">Left</option>
-              <option value="Right">Right</option>
-          </select>  {''}
-         
-          <input type="number" placeholder="Enter a number" onChange={handleNumberToKeepInputChange}/>
-          <br/>
+          <select onChange={handleLeftOrRightChange}>
+            <option value="Left">Left</option>
+            <option value="Right">Right</option>
+          </select>{" "}
+          <input
+            type="number"
+            placeholder="Enter a number"
+            onChange={handleNumberToKeepChange}
+          />
+          <br />
           <p>characters</p>
-          
-          <input type="checkbox" onChange={handleStripSpecialCharChange}/>
+          <input type="checkbox" onChange={handleStripSpecialCharChange} />
           <label>Strip Special Characters</label>
-          <br/>
-          
-          <input id="textInput" placeholder="Scan serial number here" onChange={handleTextInputChange}/>
-          
+          <br />
+          <input
+            id="textInput"
+            placeholder="Scan serial number here"
+            onChange={handleInputTextChange}
+          />
         </form>
-        
       </div>
       <div className="container">
         <h1>{parseInput(inputText)}</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
