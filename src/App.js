cb9c3989eab8d346304a9878ab46a12ddcd9e21e
Michael Marino <memarino92@gmail.com>
2021-11-02 12:13:41 -0400

    added l/r and special char parsing

--- src/App.js
+++ src/App.js
@@ -1,57 +1,81 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+  const [inputText, setInputText] = useState('')
+  const [inputNumber, setInputNumber] = useState(0)
+  const [leftOrRight, setLeftOrRight] = useState('Left')
+  const [stripSpecialChar, setStripSpecialChar] = useState(false)
+
+  const handleSideChange = (e) => {
+    setLeftOrRight(e.target.value)
+  }
+
+  const handleTextInputChange = (e) => {
+    setInputText(e.target.value)
+  }
+
+  const handleNumberInputChange = (e) => {
+    setInputNumber(e.target.value)
+  }
+
+  const handleStripSpecialCharChange = (e) => {
+    setStripSpecialChar(e.target.checked)
+  }
+  
+  //Prevent submit from clearing data
+  const preventSubmit = (e) => e.preventDefault()
+
+
+  //This is where parsing is implemented based on user-defined rules
+  const parseInput = (input) => {
+    let parsedInput = input
+
+    if (stripSpecialChar) {
+      parsedInput = parsedInput.replace(/[^a-zA-Z0-9]/g, '')
     }
-    getDate();
-  }, []);
+    if (leftOrRight == 'Left') {
+      parsedInput = parsedInput.slice(0, inputNumber)
+    } 
+    if (leftOrRight == 'Right') {
+      parsedInput = parsedInput.slice(-inputNumber)
+    }
+    
+    return parsedInput
+  }
+  
+
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
+    <>
+      <div className="container">
+        <form onSubmit={preventSubmit}>
+          
+          <p>I want to keep the</p>
+          <select name="leftOrRight" id="side" onChange={handleSideChange}>
+              <option value="Left">Left</option>
+              <option value="Right">Right</option>
+          </select>
+          
+          <input type="number" id="numberInput" placeholder="Enter a number" onChange={handleNumberInputChange}/>
+          <br/>
+          <p>characters</p>
+          
+          <input type="checkbox" id="stripSpecialChar" name="stripSpecialChar" onChange={handleStripSpecialCharChange}/>
+          <label htmlFor="stripSpecialChar">Strip Special Characters</label>
+          <br/>
+          
+          <input id="textInput" placeholder="Scan serial number here" onChange={handleTextInputChange}/>
+          
+        </form>
+        
+      </div>
+      <div className="container">
+        <h1>{parseInput(inputText)}</h1>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
